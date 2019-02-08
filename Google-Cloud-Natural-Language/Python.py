068447809abe75310fb800a9127b71b8708b4aaa
Rubens Zimbres <rubenszmm@gmail.com>
2019-02-08 17:07:14 -0200

    Create Python.py

---
+++ Google-Cloud-Natural-Language/Python.py
@@ -0,0 +1,90 @@
+#!pip install google-cloud-language
+#!pip install google-cloud-bigquery
+#!pip install google-cloud-core==0.29.0
+#!pip install six
+
+import six
+import sys
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+client = language.LanguageServiceClient()
+text = 'Leonardo da Vinci was an awesome italian genious'
+document = types.Document(
+    content=text,
+    type=enums.Document.Type.PLAIN_TEXT)
+
+sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+print('Text: {}'.format(text))
+print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+
+
+def entities_text(text):
+    client = language.LanguageServiceClient()
+    if isinstance(text, six.binary_type):
+        text = text.decode('utf-8')
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+    entities = client.analyze_entities(document).entities
+    for entity in entities:
+        entity_type = enums.Entity.Type(entity.type)
+        print('=' * 20)
+        print(u'{:<16}: {}'.format('name', entity.name))
+        print(u'{:<16}: {}'.format('type', entity_type.name))
+        print(u'{:<16}: {}'.format('metadata', entity.metadata))
+        print(u'{:<16}: {}'.format('salience', entity.salience))
+        print(u'{:<16}: {}'.format('wikipedia_url',
+              entity.metadata.get('wikipedia_url', '-')))
+        
+def syntax_text(text):
+    client = language.LanguageServiceClient()
+    if isinstance(text, six.binary_type):
+        text = text.decode('utf-8')
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+    tokens = client.analyze_syntax(document).tokens
+    for token in tokens:
+        part_of_speech_tag = enums.PartOfSpeech.Tag(token.part_of_speech.tag)
+        print(u'{}: {}'.format(part_of_speech_tag.name,
+                               token.text.content))
+def classify_text(text):
+    client = language.LanguageServiceClient()
+    if isinstance(text, six.binary_type):
+        text = text.decode('utf-8')
+    document = types.Document(
+        content=text.encode('utf-8'),
+        type=enums.Document.Type.PLAIN_TEXT)
+    categories = client.classify_text(document).categories
+    for category in categories:
+        print(u'=' * 20)
+        print(u'{:<16}: {}'.format('name', category.name))
+        print(u'{:<16}: {}'.format('confidence', category.confidence))
+
+def entity_sentiment_text(text):
+    client = language.LanguageServiceClient()
+    if isinstance(text, six.binary_type):
+        text = text.decode('utf-8')
+    document = types.Document(
+        content=text.encode('utf-8'),
+        type=enums.Document.Type.PLAIN_TEXT)
+    encoding = enums.EncodingType.UTF32
+    if sys.maxunicode == 65535:
+        encoding = enums.EncodingType.UTF16
+    result = client.analyze_entity_sentiment(document, encoding)
+    for entity in result.entities:
+        print('Mentions: ')
+        print(u'Name: "{}"'.format(entity.name))
+        for mention in entity.mentions:
+            print(u'  Begin Offset : {}'.format(mention.text.begin_offset))
+            print(u'  Content : {}'.format(mention.text.content))
+            print(u'  Magnitude : {}'.format(mention.sentiment.magnitude))
+            print(u'  Sentiment : {}'.format(mention.sentiment.score))
+            print(u'  Type : {}'.format(mention.type))
+        print(u'Salience: {}'.format(entity.salience))
+        print(u'Sentiment: {}\n'.format(entity.sentiment))
+
+entity_sentiment_text('As he crossed toward the pharmacy at the corner he involuntarily turned his head because of a burst of light that had ricocheted from his temple, and saw, with that quick smile with which we greet a rainbow or a rose, a blindingly white parallelogram of sky being unloaded from the van')
