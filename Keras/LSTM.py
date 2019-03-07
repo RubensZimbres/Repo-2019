739f88da83b48f123a8904a112b6274bd01c8021
Rubens Zimbres <rubenszmm@gmail.com>
2019-03-07 14:09:00 -0300

    Update LSTM.py

--- Keras/LSTM.py
+++ Keras/LSTM.py
@@ -31,31 +31,30 @@ from keras.models import Model
 from keras import backend as K
 from keras.optimizers import SGD
 
-learning_rate = 0.015
+learning_rate = 0.01
 decay_rate = 5e-6
-momentum = 0.9
+momentum = 0.65
 
 data_dim = 5
 timesteps = 1
 num_classes = 10
-batchsize=300
+batchsize=200
 
 input_img = Input(batch_shape=(None, 1,5))
-model1=LSTM(12, return_sequences=False,input_shape=(timesteps, data_dim))(input_img)
-model4=Dense(5, activation='relu')(model1)
-model5 = Model(inputs = input_img, outputs = model4)
+model1=LSTM(128, return_sequences=True,input_shape=(timesteps, data_dim))(input_img)
+model2=LSTM(128,return_sequences=False)(model1)
+model4=Dense(5, activation='relu')(model2)
+model = Model(inputs = input_img, outputs = model4)
 
 sgd = SGD(lr=learning_rate,momentum=momentum, decay=decay_rate, nesterov=False)
 
 
-model5.compile(loss="mean_squared_error", optimizer="rmsprop",metrics=['accuracy'])  
+model.compile(loss="mean_squared_error", optimizer="rmsprop",metrics=['mae'])  
 
-model5.summary()
+model.summary()
 
-epochs_=100
+epochs_=60
 
-model5.fit(X0.reshape(-1,1,5).astype("float"),Y0.astype("float"), batch_size=batchsize, epochs=epochs_,verbose=1)  
+model.fit(X0.reshape(-1,1,5).astype("float"),Y0.astype("float"), batch_size=batchsize, epochs=epochs_,verbose=1)  
 
-model5.predict(np.array(dados).reshape(-1,1,5).astype("float"))
-
-
+model.predict(np.array(dados).reshape(-1,1,5).astype("float"))
