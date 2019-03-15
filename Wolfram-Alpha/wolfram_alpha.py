6e1570f7d9159dc313e82d0d3777b1bda2286407
Rubens Zimbres <rubenszmm@gmail.com>
2019-03-15 16:50:39 -0300

    Update wolfram_alpha.py

--- Wolfram-Alpha/wolfram_alpha.py
+++ Wolfram-Alpha/wolfram_alpha.py
@@ -3,6 +3,8 @@ client = wolframalpha.Client("AB1234-12345ABCD")
 
 res = client.query('temperature in Los Angeles, CA on October 12, 2000')
 
+next(res.results).text
+
 for pod in res.pods:
     for sub in pod.subpods:
         print(sub.text)
