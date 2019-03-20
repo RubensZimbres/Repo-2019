b901d3a7ef6c7c3d59f546b64d3fd7af3b0bf0d0
Rubens Zimbres <rubenszmm@gmail.com>
2019-03-20 13:53:28 -0300

    Update Tensorflow_Probability.py

--- Tensorflow/Tensorflow_Probability.py
+++ Tensorflow/Tensorflow_Probability.py
@@ -11,15 +11,16 @@ dataframe.head()
 
 plt.plot(range(0,dataframe.shape[0]),dataframe.iloc[:,1])
 
-x1=np.array(dataframe.iloc[:,1]+np.random.randn(dataframe.shape[0])).astype(np.float32).reshape(-1,1)
+x1=np.array(dataframe.iloc[:,1]+np.random.randn(dataframe.shape[0])).astype(np.float32).reshape(-1,1)[0:500]
 
-y=np.array(dataframe.iloc[:,1]).T.astype(np.float32).reshape(-1,1)
+y=np.array(dataframe.iloc[:,1]).T.astype(np.float32).reshape(-1,1)[150:650]
 
 tfd = tfp.distributions
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
+    sess.run(init)
 
     model = tf.keras.Sequential([
       tf.keras.layers.Dense(1,kernel_initializer='glorot_uniform'),
@@ -31,22 +32,26 @@ with tf.Session() as sess:
     
     model.fit(x1,y, epochs=500, verbose=True, batch_size=16)
     
-    yhat = model(x1)
-    mean = yhat.mean()
-
-
-    sess.run(init)
-    mm = sess.run(mean)    
-    mean = yhat.mean()
+    yhat = model(np.array(dataframe.iloc[:,1]).T.astype(np.float32).reshape(-1,1)[650:800])
+    mean0 = tf.convert_to_tensor(yhat)
+    mean = sess.run(mean0)    
     stddev = yhat.stddev()
-    mean_plus_2_std = sess.run(mean - 2. * stddev)
-    mean_minus_2_std = sess.run(mean + 2. * stddev)
+    mean_plus_2_std = sess.run(mean - 3. * stddev)
+    mean_minus_2_std = sess.run(mean + 3. * stddev)
 
+mean.shape
 
-plt.figure(figsize=(10,7))
+plt.figure(figsize=(12,9))
 plt.plot(y,color='red',linewidth=1,label='Real Data')
 #plt.plot(mm,color='green',linewidth=1,label='Real Data')
 plt.plot(np.concatenate([y,mean_minus_2_std],axis=0),color='blue',linewidth=0.6,label='Predicted + 2 std dev')
 plt.plot(np.concatenate([y,mean_plus_2_std],axis=0),color='green',linewidth=0.6,label='Predicted - 2 std dev')
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(12,9))
+plt.plot(mean,color='black',linewidth=3,label='Predicted + 2 std dev')
+plt.plot(mean_minus_2_std,color='blue',linewidth=0.6,label='Predicted + 2 std dev')
+plt.plot(mean_plus_2_std,color='green',linewidth=0.6,label='Predicted - 2 std dev')
 plt.legend()
 plt.show()
