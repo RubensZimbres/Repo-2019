9bce7f5d78c6f1ee9b8a8f8a10f517f0c180940c
Rubens Zimbres <rubenszmm@gmail.com>
2019-03-25 09:37:20 -0300

    Create Insert_Data_SQL_pyodbc.py

---
+++ Microsoft-SQL-Server/Insert_Data_SQL_pyodbc.py
@@ -0,0 +1,25 @@
+import pyodbc 
+
+server = 'tcp:instance.abcd12345.rds.amazonaws.com'
+database = 'database4'
+username = 'your_username'
+password = 'your_password'
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
+cursor = cnxn.cursor()
+
+cursor.execute('''
+
+                INSERT INTO TabelaDados(ID,LastName,FirstName,Address,City)
+VALUES ('128', 'Marques', 'Maria','Av Faria Lima, 777', 'São Paulo');
+
+                ''')
+
+cnxn.commit()
+
+cursor.execute("SELECT * FROM Database4.dbo.TabelaDados;")
+
+row = cursor.fetchone()
+while row:
+    print(row)
+    row = cursor.fetchone()
