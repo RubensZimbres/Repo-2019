f76df56a12451d4444a395f8392392427b041e30
Rubens Zimbres <rubenszmm@gmail.com>
2019-05-23 17:39:01 -0300

    Create request.py

---
+++ Google-Cloud-Functions/request.py
@@ -0,0 +1,22 @@
+import requests
+
+KERAS_REST_API_URL = "https://us-central2-project02-12345.cloudfunctions.net/test-function"
+
+from google.cloud import storage
+
+client = storage.Client()
+
+bucket = client.get_bucket('images22')
+
+blob = bucket.get_blob('dog.jpg')
+
+data = blob.download_to_filename('dog22.jpg')
+
+with open("dog22.jpg", 'rb') as f:
+    image=f.read()
+
+payload = {"image": image}
+
+r = requests.post(KERAS_REST_API_URL, files=payload).json()
+
+print(r)
