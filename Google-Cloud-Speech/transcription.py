7bd350073c907e7d61c26830df49f78335a77cb7
Rubens Zimbres <rubenszmm@gmail.com>
2019-06-26 11:24:37 -0300

    Create transcription.py

---
+++ Google-Cloud-Speech/transcription.py
@@ -0,0 +1,29 @@
+from google.cloud import speech_v1p1beta1 as speech
+
+client = speech.SpeechClient()
+
+config = speech.types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=8000,
+    enable_word_time_offsets= True,
+    language_code='pt-BR',
+    enable_automatic_punctuation= True,
+    use_enhanced=True,
+    speech_contexts=[speech.types.SpeechContext(phrases=['boleto','nome','telefone', 'endereço','senhor','senhora'])],
+    enable_speaker_diarization=False,
+#   diarization_speaker_count=2,
+    audio_channel_count=2,
+    profanity_filter=False,
+    enable_separate_recognition_per_channel=True)
+
+audio=speech.types.RecognitionAudio(uri="gs://folder/ggf_output.wav")
+
+operation = client.long_running_recognize(config, audio)
+
+print('Waiting for operation to complete...')
+
+response = operation.result(timeout=10000)
+
+
+for i in range(0,len(response.results)):
+    print('Speaker:',response.results[i].channel_tag,response.results[i].alternatives[0].transcript)
