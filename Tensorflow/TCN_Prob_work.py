80247a3085ffd399594a5a657c2ca7d1a30ff905
Rubens Zimbres <rubenszmm@gmail.com>
2019-07-11 15:52:13 -0300

    Create TCN_Prob_work.py

---
+++ Tensorflow/TCN_Prob_work.py
@@ -0,0 +1,246 @@
+import tensorflow as tf
+import pandas as pd  
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow_probability as tfp
+
+dataframe = pd.read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+
+look_back=1
+np.random.seed(7)
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+train_size = int(len(dataset) * 0.8)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
+
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+trainX
+
+trainY = trainY.reshape(len(trainY), 1)
+testY = testY.reshape(len(testY), 1)
+trainY
+
+X0=trainX
+Y0=trainY
+
+tfd = tfp.distributions
+
+class TemporalConvNet(tf.layers.Layer):
+    def __init__(self, num_channels, kernel_size=2, dropout=0.2,
+                 trainable=True, name=None, dtype=None, 
+                 activity_regularizer=None, **kwargs):
+        super(TemporalConvNet, self).__init__(
+            trainable=trainable, dtype=dtype,
+            activity_regularizer=activity_regularizer,
+            name=name, **kwargs
+        )
+        self.layers = []
+        num_levels = len(num_channels)
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            out_channels = num_channels[i]
+            self.layers.append(
+                TemporalBlock(out_channels, kernel_size, strides=1, dilation_rate=dilation_size,
+                              dropout=dropout, name="tblock_{}".format(i))
+            )
+    
+    def call(self, inputs, training=True):
+        outputs = inputs
+        for layer in self.layers:
+            outputs = layer(outputs, training=training)
+        return outputs
+
+learning_rate = 0.001
+display_step = 10
+num_input = 1
+num_hidden = 20
+num_classes = 1
+
+dropout = 0.1
+kernel_size = 8
+levels = 6
+
+class CausalConv1D(tf.layers.Conv1D):
+    def __init__(self, filters,
+               kernel_size,
+               strides=1,
+               dilation_rate=1,
+               activation=None,
+               use_bias=True,
+               kernel_initializer=None,
+               bias_initializer=tf.zeros_initializer(),
+               kernel_regularizer=None,
+               bias_regularizer=None,
+               activity_regularizer=None,
+               kernel_constraint=None,
+               bias_constraint=None,
+               trainable=True,
+               name=None,
+               **kwargs):
+        super(CausalConv1D, self).__init__(
+            filters=filters,
+            kernel_size=kernel_size,
+            strides=strides,
+            padding='valid',
+            data_format='channels_last',
+            dilation_rate=dilation_rate,
+            activation=activation,
+            use_bias=use_bias,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer,
+            kernel_regularizer=kernel_regularizer,
+            bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer,
+            kernel_constraint=kernel_constraint,
+            bias_constraint=bias_constraint,
+            trainable=trainable,
+            name=name, **kwargs
+        )
+       
+    def call(self, inputs):
+        padding = (self.kernel_size[0] - 1) * self.dilation_rate[0]
+        inputs = tf.pad(inputs, tf.constant([(0, 0,), (1, 0), (0, 0)]) * padding)
+        return super(CausalConv1D, self).call(inputs)
+
+
+class TemporalBlock(tf.layers.Layer):
+    def __init__(self, n_outputs, kernel_size, strides, dilation_rate, dropout=0.2, 
+                 trainable=True, name=None, dtype=None, 
+                 activity_regularizer=None, **kwargs):
+        super(TemporalBlock, self).__init__(
+            trainable=trainable, dtype=dtype,
+            activity_regularizer=activity_regularizer,
+            name=name, **kwargs
+        )        
+        self.dropout = dropout
+        self.n_outputs = n_outputs
+        self.conv1 = CausalConv1D(
+            n_outputs, kernel_size, strides=strides, 
+            dilation_rate=dilation_rate, activation=tf.nn.relu, 
+            name="conv1")
+        self.conv2 = CausalConv1D(
+            n_outputs, kernel_size, strides=strides, 
+            dilation_rate=dilation_rate, activation=tf.nn.relu, 
+            name="conv2")
+        self.down_sample = None
+
+    
+    def build(self, input_shape):
+        channel_dim = 2
+        self.dropout1 = tf.layers.Dropout(self.dropout, [tf.constant(1), tf.constant(1), tf.constant(self.n_outputs)])
+        self.dropout2 = tf.layers.Dropout(self.dropout, [tf.constant(1), tf.constant(1), tf.constant(self.n_outputs)])
+        if input_shape[channel_dim] != self.n_outputs:
+            self.down_sample = tf.layers.Dense(self.n_outputs, activation=None)
+    
+    def call(self, inputs, training=True):
+        x = self.conv1(inputs)
+        x = tf.contrib.layers.layer_norm(x)
+        x = self.dropout1(x, training=training)
+        x = self.conv2(x)
+        x = tf.contrib.layers.layer_norm(x)
+        x = self.dropout2(x, training=training)
+        if self.down_sample is not None:
+            inputs = self.down_sample(inputs)
+        return tf.nn.relu(x + inputs)
+
+
+
+tf.reset_default_graph()
+graph = tf.Graph()
+with graph.as_default():
+    tf.set_random_seed(10)
+    
+    X = tf.placeholder("float", [None, look_back, num_input])
+    Y = tf.placeholder("float", [None, num_classes])
+    is_training = tf.placeholder("bool")
+    
+    logits = tf.layers.dense(
+        TemporalConvNet([num_hidden] * levels, kernel_size, dropout)(
+            X, training=is_training)[:, -1, :],
+        num_classes, activation=None, 
+        kernel_initializer=tf.orthogonal_initializer()
+    )
+    prediction = tf.nn.relu(logits)
+   
+    loss_op = tf.losses.mean_squared_error(
+        labels=Y,predictions=prediction)
+    
+    accuracy,acc_op=tf.metrics.accuracy(labels=Y,predictions=prediction)
+
+    with tf.name_scope("optimizer"):
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        train_op = optimizer.minimize(loss_op)
+
+
+    saver = tf.train.Saver()
+    print("All parameters:", np.sum([np.product([xi.value for xi in x.get_shape()]) for x in tf.global_variables()]))
+    print("Trainable parameters:", np.sum([np.product([xi.value for xi in x.get_shape()]) for x in tf.trainable_variables()]))
+
+def next_batch(num, data, labels):
+    idx = np.arange(0 , len(data))
+    np.random.shuffle(idx)
+    idx = idx[:num]
+    data_shuffle = [data[ i] for i in idx]
+    labels_shuffle = [labels[ i] for i in idx]
+    return np.asarray(data_shuffle).astype(np.float32), np.asarray(labels_shuffle).astype(np.float32)
+
+log_dir = "/home/rubens/Documents/Dados/"
+tb_writer = tf.summary.FileWriter(log_dir, graph)
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = False
+config.gpu_options.per_process_gpu_memory_fraction = 0.7
+best_val_acc = 0.85
+
+training_epochs = 1000
+batch_size = 60
+total_batch = int(Y0.shape[1] / batch_size)
+
+X0=X0.reshape(-1,1,1)
+testX=testX.reshape(-1,1,1)
+
+with tf.Session(graph=graph, config=config) as sess:
+    init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    sess.run(init)
+    for step in range(1, training_epochs+1):
+        Xt, Yt = next_batch(batch_size, X0, Y0)
+        batch_x, batch_y = Xt,Yt
+        sess.run(train_op, feed_dict={X: batch_x, Y: batch_y, is_training: True})
+        if step % display_step == 0 or step == 1:
+            loss, acc = sess.run([loss_op, accuracy], feed_dict={
+                X: batch_x, Y: batch_y, is_training: False})
+            test_len = 100
+            test_data = testX
+            test_label = testY
+            val_acc = sess.run(accuracy, feed_dict={X: test_data, Y: test_label, is_training: False})
+            print("Step " + str(step) + ", Minibatch Loss= " + \
+                  "{:.4f}".format(loss) + ", Training Accuracy= " + \
+                  "{:.4f}".format(acc) + " + , Test Accuracy= " + \
+                  "{:.4f}".format(val_acc))
+            print(acc)
+            if val_acc > best_val_acc:
+                best_val_acc = val_acc
+                save_path = saver.save(sess, "/home/rubens/Documents/Dados/model.ckpt")
+                print("Model saved in path: %s" % save_path)
+    pred00 = sess.run([prediction],feed_dict={X: x_test_ok, is_training: False})
+
+
+with tf.Session(graph=graph, config=config) as session:
+    ckpt = "/home/rubens/Documents/Dados/model.ckpt"
+    saver.restore(session, ckpt)
+    pred00 = session.run([prediction], feed_dict={X: x_test_ok, is_training: False})
+
+pred00-testY
+
