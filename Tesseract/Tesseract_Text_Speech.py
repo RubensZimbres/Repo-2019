308e02d6fa9c244c33b873db79fb4e5702cecc35
Rubens Zimbres <rubenszmm@gmail.com>
2019-07-23 14:33:30 -0300

    Update Tesseract_Text_Speech.py

--- Tesseract/Tesseract_Text_Speech.py
+++ Tesseract/Tesseract_Text_Speech.py
@@ -1,24 +1,23 @@
 from PIL import Image
 import pytesseract
 import argparse
-import cv2
 import os
 
-im = cv2.imread('/home/rubens/deeplearningbook.png', cv2.IMREAD_COLOR)
-config = ('-l eng --oem 1 --psm 3')
-text = pytesseract.image_to_string(im, config=config)
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+import numpy 
+import matplotlib.pyplot as plt
+
+im = plt.imread('/home/rubens/file5_deep.tiff')
+
+config = ('-l eng --oem 3 --psm 10')
+text00 = pytesseract.image_to_string(im, config=config)
  
 print(text)
 
-filename='/home/rubens/test1.tiff'
-
-text00 = pytesseract.image_to_string(Image.open(filename))
-
-print(text[0:1000])
- 
-#cv2.imshow("Image", image)
-#cv2.imshow("Output", gray)
-#cv2.waitKey(0)
 
 # ! pip install --upgrade google-cloud-texttospeech
 
