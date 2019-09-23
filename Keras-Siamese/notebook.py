e3ee546cb13b12ffb4b85b970fff04ebeddf3155
Rubens Zimbres <rubenszmm@gmail.com>
2019-09-23 14:21:58 -0300

    Update notebook.py

--- Keras-Siamese/notebook.py
+++ Keras-Siamese/notebook.py
@@ -19,7 +19,7 @@ from keras.optimizers import SGD
 with session.as_default():
     with session.graph.as_default():
         
-        epochs=2500
+        epochs=20
         learning_rate = 0.01
         decay_rate = 5e-5
         momentum = 0.6
@@ -107,19 +107,28 @@ with session.as_default():
 
         model.compile(loss='categorical_crossentropy', optimizer=sgd,metrics = ['accuracy'])
 
-        model.summary()        
+        model.summary()
         
 from keras.utils import plot_model 
 plot_model(model, to_file='model_gif.png')
 
+from sklearn.model_selection import train_test_split
+
+X, y = x_train0, target
+X1, y1 = x_train1, target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train1, X_test1, y_train1, y_test1 = train_test_split(X1, y1, test_size=0.1, random_state=42)
+concat_train,concat_test, y_train2, y_test2=train_test_split(concat, y1, test_size=0.1, random_state=42)
+
 with tf.Session(graph=graph, config=config) as session:
     init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
     session.run(init)
-    model.fit([X_train, X_train1],y_train,
+    model.fit([X_train, X_train1,concat_train],y_train,
                 nb_epoch=epochs,
                 batch_size=2771,verbose=1,validation_split=0.1)
-    preds=model.predict([X_test,X_test1])
-
+    preds=model.predict([X_test,X_test1,concat_test])
+    
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score,precision_score,confusion_matrix,f1_score,recall_score
 
