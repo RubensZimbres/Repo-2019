bd28b2af49933b0198d42faddd37afe038d4927a
Rubens Zimbres <rubenszmm@gmail.com>
2019-10-22 10:13:47 -0300

    Update canny.py

--- OpenCV/Canny_Edge/canny.py
+++ OpenCV/Canny_Edge/canny.py
@@ -2,10 +2,9 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-filename = "imagem.png"
+filename = "three.jpg"
 src = cv2.imread(filename)
 
-#from __future__ import print_function
 max_lowThreshold = 100
 window_name = 'Edge Map'
 title_trackbar = 'Min Threshold:'
@@ -13,13 +12,13 @@ ratio = 3
 kernel_size = 3
 def CannyThreshold(val):
     low_threshold = val
-    img_blur = cv.blur(src_gray, (3,3))
-    detected_edges = cv.Canny(img_blur, low_threshold, low_threshold*ratio, kernel_size)
+    img_blur = cv2.blur(src_gray, (3,3))
+    detected_edges = cv2.Canny(img_blur, low_threshold, low_threshold*ratio, kernel_size)
     mask = detected_edges != 0
     dst = src * (mask[:,:,None].astype(src.dtype))
-    cv.imshow(window_name, dst)
-src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-cv.namedWindow(window_name)
-cv.createTrackbar(title_trackbar, window_name , 0, max_lowThreshold, CannyThreshold)
+    cv2.imshow(window_name, dst)
+src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+cv2.namedWindow(window_name)
+cv2.createTrackbar(title_trackbar, window_name , 0, max_lowThreshold, CannyThreshold)
 CannyThreshold(0)
-cv.waitKey()
+cv2.waitKey()
