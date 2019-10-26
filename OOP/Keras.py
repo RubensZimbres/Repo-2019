0dcbaed487457a140d67f9ebb504264f86104a67
Rubens Zimbres <rubenszmm@gmail.com>
2019-10-26 20:12:30 -0300

    Create Keras.py

---
+++ OOP/Keras.py
@@ -0,0 +1,147 @@
+#!pip install tf-nightly-gpu-2.0-preview --user
+
+from tensorflow.keras.layers import Layer
+import tensorflow as tf
+
+
+class Linear(Layer):
+  """y = w.x + b"""
+
+  def __init__(self, units=32):
+      super(Linear, self).__init__()
+      self.units = units
+
+  def build(self, input_shape):
+      self.w = self.add_weight(shape=(input_shape[-1], self.units),
+                               initializer='random_normal',
+                               trainable=True)
+      self.b = self.add_weight(shape=(self.units,),
+                               initializer='random_normal',
+                               trainable=True)
+
+  def call(self, inputs):
+      return tf.matmul(inputs, self.w) + self.b
+
+
+linear_layer = Linear(6)
+
+y = linear_layer(tf.ones((2, 2)))
+y
+
+
+''' DATASET '''
+
+(x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
+dataset = tf.data.Dataset.from_tensor_slices(
+    (x_train.reshape(60000, 784).astype('float32') / 255, y_train))
+dataset = dataset.shuffle(buffer_size=1024).batch(64)
+
+linear_layer = Linear(10)
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
+
+'''TRAIN'''
+
+for step, (x, y) in enumerate(dataset):
+  
+    with tf.GradientTape() as tape:
+
+        logits = linear_layer(x)
+
+        loss = loss_fn(y, logits)
+     
+    gradients = tape.gradient(loss, linear_layer.trainable_weights)
+  
+    optimizer.apply_gradients(zip(gradients, linear_layer.trainable_weights))
+  
+    if step % 100 == 0:
+        print(step, float(loss))
+        
+###################################################################################################        
+        
+        
+'''MLP'''
+class BahdanauAttention(Layer):
+  def __init__(self, units):
+    super(BahdanauAttention, self).__init__()
+    self.W1 = tf.keras.layers.Dense(units)
+    self.W2 = tf.keras.layers.Dense(units)
+    self.V = tf.keras.layers.Dense(1)
+
+  def call(self, values):
+    # hidden shape == (batch_size, hidden size)
+    # hidden_with_time_axis shape == (batch_size, 1, hidden size)
+    # we are doing this to perform addition to calculate the score
+    hidden_with_time_axis = tf.expand_dims(values, 1)
+    # score shape == (batch_size, max_length, 1)
+    # we get 1 at the last axis because we are applying score to self.V
+    # the shape of the tensor before applying self.V is (batch_size, max_length, units)
+    score = self.V(tf.nn.tanh(
+        self.W1(values) + self.W2(hidden_with_time_axis)))
+
+    # attention_weights shape == (batch_size, max_length, 1)
+    attention_weights = tf.nn.softmax(score, axis=1)
+
+    # context_vector shape after sum == (batch_size, hidden_size)
+    context_vector = attention_weights * values
+    context_vector = tf.reduce_sum(context_vector, axis=1)
+
+    return context_vector
+
+
+class MLP(Layer):
+    """Simple stack of Linear layers."""
+
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.linear_1 = Linear(32)
+        self.attention=BahdanauAttention(10)
+        self.linear_2 = Linear(32)
+        self.linear_3 = Linear(10)
+
+    def call(self, inputs):
+        x = self.linear_1(inputs)
+        x = tf.nn.relu(x)
+        x = self.attention(x)
+        x = tf.nn.relu(x)
+        x = self.linear_2(x)
+        x = tf.nn.relu(x)
+        return self.linear_3(x)
+
+mlp = MLP()
+
+
+(x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
+dataset = tf.data.Dataset.from_tensor_slices(
+    (x_train.reshape(60000, 784).astype('float32') / 255, y_train))
+dataset = dataset.shuffle(buffer_size=1024).batch(64)
+
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
+
+
+
+for step, (x, y) in enumerate(dataset):
+  
+    for i in range(0,30):
+        with tf.GradientTape() as tape:
+
+            logits = mlp(x)
+    
+            loss = loss_fn(y, logits)
+         
+        gradients = tape.gradient(loss, mlp.trainable_weights)
+      
+        optimizer.apply_gradients(zip(gradients, mlp.trainable_weights))
+      
+        if step % 100 == 0:
+            print(step, float(loss))
+
+    print(y-tf.cast(tf.argmax(logits,axis=1),tf.uint8))
+    
+    
+    
