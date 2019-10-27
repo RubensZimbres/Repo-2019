583868ab398f2ebf28ed6344d8485f82b7057db8
Rubens Zimbres <rubenszmm@gmail.com>
2019-10-27 16:30:55 -0300

    Update Python.py

--- OOP/Python.py
+++ OOP/Python.py
@@ -36,3 +36,58 @@ cube=Cube(3)
 cube.surface_area()
 
 cube.volume()        
+
+
+class Point:
+    def reset(self):
+        self.x=0
+        self.y=0
+    
+p=Point()
+p.reset()
+
+p.x
+
+
+import math
+class Point:
+    def move(self,x,y):
+        self.x=x
+        self.y=y
+        
+    def reset(self):
+        self.move(0,0)
+        
+    def distance(self,other):
+        return math.sqrt((self.x-other.x)**2-(self.y-other.y)**2)
+    
+p1=Point()
+p2=Point()
+
+p1.reset()
+
+p2.move(5,0)
+
+p1.distance(p2)
+
+
+import math
+class Point:
+    def __init__(self,x,y):
+        '''Initialize the position'''
+        self.move(x,y)
+    
+    def move(self,x,y):
+        '''Move to new location'''
+        self.x=x
+        self.y=y
+        
+    def reset(self):
+        self.move(0,0)
+        
+    def distance(self,other):
+        return math.sqrt((self.x-other.x)**2-(self.y-other.y)**2)
+
+p=Point(3,1)
+
+p.distance(p2)
