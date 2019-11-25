0a9c729576983381657c5bff51152fe558ae2261
Rubens Zimbres <rubenszmm@gmail.com>
2019-11-25 15:00:41 -0300

    Update parse_json.py

--- BERT_Embeddings_PyTorch/embed_tsv/parse_json.py
+++ BERT_Embeddings_PyTorch/embed_tsv/parse_json.py
@@ -1,14 +1,21 @@
 import json
 from pprint import pprint
 import numpy as np
+import pandas as pd
 
-data = [json.loads(line) for line in open('texto3.json', 'r')]
+data = [json.loads(line) for line in open('gensim.json', 'r')]
 
 xx=[]
 for parte in range(0,len(data)):
     xx.append(np.mean([data[parte]['features'][i]['layers'][0]['values'] for i in range(0,len(data[parte]['features']))],axis=0))
 
-print(len(xx[0]))
+from scipy.spatial.distance import cosine as cos
 
-#linex_index": 0, "features":
-#{"token": "uma", "layers": [{"index": -1, "values": 
+df=pd.read_csv('gensim.csv',encoding = "latin-1",header=None)
+print(df.shape)
+
+print(len(data))
+
+for i in range(0,len(xx)):
+    print(np.array(df)[i],cos(xx[3],xx[i]))
+
