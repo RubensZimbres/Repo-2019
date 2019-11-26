8482cbe055400b481b218d869635a3f1a8ef0223
Rubens Zimbres <rubenszmm@gmail.com>
2019-11-26 16:51:40 -0300

    Create run_gen_nb.py

---
+++ Text_Generation/run_gen_nb.py
@@ -0,0 +1,17 @@
+# weights at https://huggingface.co/transformers/pretrained_models.html
+
+import run_generation as gen
+from transformers import *
+
+c=gen.Main()
+
+MODEL_CLASSES = {
+    'gpt2': (GPT2LMHeadModel, GPT2Tokenizer),
+    'ctrl': (CTRLLMHeadModel, CTRLTokenizer),
+    'openai-gpt': (OpenAIGPTLMHeadModel, OpenAIGPTTokenizer),
+    'xlnet': (XLNetLMHeadModel, XLNetTokenizer),
+    'transfo-xl': (TransfoXLLMHeadModel, TransfoXLTokenizer),
+    'xlm': (XLMWithLMHeadModel, XLMTokenizer),
+}
+
+c.main(model_type='gpt2',model_name_or_path='gpt2-large',length=200, temperature=1)
