329eebb6f84b5062c1728cc8b5952b34d4e844a3
Rubens Zimbres <rubenszmm@gmail.com>
2019-12-07 18:01:10 -0300

    Create run_gen.nb.py

---
+++ OpenAI-GPT-2/run_gen.nb.py
@@ -0,0 +1,15 @@
+import run_generation as gen
+from transformers import *
+
+c=gen.Main()
+
+MODEL_CLASSES = {
+    'gpt2': (GPT2LMHeadModel, GPT2Tokenizer),
+    'ctrl': (CTRLLMHeadModel, CTRLTokenizer),
+    'openai-gpt': (OpenAIGPTLMHeadModel, OpenAIGPTTokenizer),
+    'xlnet': (XLNetLMHeadModel, XLNetTokenizer),
+    'transfo-xl': (TransfoXLLMHeadModel, TransfoXLTokenizer),
+    'xlm': (XLMWithLMHeadModel, XLMTokenizer),
+}
+
+c.main(model_type='gpt2',model_name_or_path='gpt2-large',length=200, temperature=1)
